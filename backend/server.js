246a8468fa293282bbb2a239f4b04c728f7d43ab
Mai Kanetaka <maikanetaka@gmail.com>
2024-05-20 23:18:04 +0200

    Use environment variable for secret

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import cors from "cors"; // Approves requests from other domains
 import express from "express"; // Framework to create a server
 import mongoose from "mongoose"; // To access MongoDB
 import bcrypt from "bcrypt"; // Library to hash passwords safely
-import jwt from "jsonwebtoken"; // Library to create a token for authenticating users
+import jwt from "jsonwebtoken"; // Library to create a token for authenticating users - usually put in cookie
 
 // Set the URL to connect MongoDB. If the environment variable is not set, connect to the local MongoDB instance.
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
@@ -16,17 +16,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Define the user schema / how the data is structured
+// Define the user schema / how the data is structured for mongo
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   token: { type: String },
 });
 
-// Hashing the password before the user is saved
+// Hashing the password before the user is saved (mongo thing)
 userSchema.pre("save", async function (next) {
   if (this.isModified("password") || this.isNew) {
-    this.password = await bcrypt.hash(this.password, 10); // Fix typo: 'passwrod' to 'password'
+    this.password = await bcrypt.hash(this.password, 10);
   }
   next();
 });
@@ -39,7 +39,7 @@ app.post("/register", async (req, res) => {
   try {
     const { username, password } = req.body; // Getting username and password info from req.body
     const user = new User({ username, password }); // Create new user
-    await user.save(); // Save user info in the database
+    await user.save(); // Here, finally save user info in the database
     res.status(201).send("User registered successfully");
   } catch (error) {
     res.status(400).send("Error registering user");
@@ -49,12 +49,13 @@ app.post("/register", async (req, res) => {
 // Endpoint - login
 app.post("/login", async (req, res) => {
   try {
-    const { username, password } = req.body;
+    const { username, password } = req.body; // body contains post request data (user's input)
     const user = await User.findOne({ username }); // search user by username
     if (!user || !(await bcrypt.compare(password, user.password))) {
+      //(right one, user's input)
       return res.status(401).send("Invalid username or password.");
     }
-    const token = jwt.sign({ userId: user._id }, "your_secret_key", {
+    const token = jwt.sign({ userId: user._id }, process.env.SECRET, {
       //create JWT token
       expiresIn: "1h",
     });
@@ -71,7 +72,7 @@ const authenticateToken = (req, res, next) => {
   const token = req.headers["authorization"]; // Get token from request headers
   if (!token) return res.status(401).send("Access denied"); // If no token, deny access
 
-  jwt.verify(token, "your_secret_key", (err, user) => {
+  jwt.verify(token, process.env.SECRET, (err, user) => {
     // Verify token
     if (err) return res.status(403).send("Invalid token"); // If token is invalid, deny access
     req.user = user; // Add user info to request
